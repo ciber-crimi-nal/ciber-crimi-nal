2585be4c060b2e6e0c96b6a0ce3478e3dc51658d
ac4lv0p1 <dnis.incibe@gmail.com>
2025-09-24 11:36:17 +0200

    <feature/dni-censorship>: add output image generation

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import os
 import cv2
 from scripts.dni_detector import read_dni
 
-sides = ["front"]
+sides = ["front","back"]
+ruta = ["images/sintetico.png", "images/sintetico_rever.png"]
+
 
 print("************ CENSURAR yolo + homografia + opencv ************")
-ruta = ["images/real.jpeg"]
-# ruta = ["images/sintetico.png"]
 dni_img_censurado = read_dni(ruta, sides, debug=False)
-if dni_img_censurado is not None:
-    cv2.imwrite("output/dni_censurado2.png", dni_img_censurado)
+if dni_img_censurado is not None and isinstance(dni_img_censurado, dict):
+    
+    name = os.path.basename(ruta[0]).split(".")[0]
+
+    for side, img in dni_img_censurado.items():
+        if img is not None:
+            print(side)
+
+            output = os.path.join("output", f"{name}_{side}_censurado.png")
+            cv2.imwrite(output, img)
 else:
     print("La imagen censurada está vacía.")
