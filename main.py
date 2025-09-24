e7e32c2cfcc1f85643837cc8240f1b586624f051
ac4lv0p1 <dnis.incibe@gmail.com>
2025-09-24 13:52:41 +0200

    <feature/dni-censorship>: add watermark input

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import cv2
 from scripts.dni_detector import read_dni
+from scripts.utils import pdf_generator
 
 sides = ["front","back"]
 ruta = ["images/sintetico.png", "images/sintetico_rever.png"]
+images_for_pdf = []
 
 
 print("************ CENSURAR yolo + homografia + opencv ************")
@@ -14,9 +16,12 @@ if dni_img_censurado is not None and isinstance(dni_img_censurado, dict):
 
     for side, img in dni_img_censurado.items():
         if img is not None:
-            print(side)
-
             output = os.path.join("output", f"{name}_{side}_censurado.png")
             cv2.imwrite(output, img)
+            images_for_pdf.append(img)
+
+    if images_for_pdf:
+        pdf_generator(images_for_pdf, name, "marca de agua")
+
 else:
     print("La imagen censurada está vacía.")
